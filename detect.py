1554153327959b375b440b46b8d560e119600a2c
Erwan Velu <erwan@enovance.com>
2013-06-14 17:21:27 +0200

    detect: Adding link attribute
    Let's report the ethernet link status

--- detect.py
+++ detect.py
@@ -101,6 +101,9 @@ def detect_system(l):
                 ip = e.find("configuration/setting[@id='ip']")
                 if ip is not None:
                     l.append(('network', name.text, 'ipv4', ip.attrib['value']))
+                link = e.find("configuration/setting[@id='link']")
+                if link is not None:
+                    l.append(('network', name.text, 'link', link.attrib['value']))
 
     status, output = commands.getstatusoutput('nproc')
     if status == 0:

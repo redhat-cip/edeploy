82c10888f773b8c4637d2b9523a3d51d41925df8
Frederic Lepied <frederic.lepied@enovance.com>
2013-06-20 20:38:38 +0200

    add license

--- policy-rc.d
+++ policy-rc.d
@@ -1,12 +1,24 @@
 #!/bin/bash
+#
+# Copyright (C) 2013 eNovance SAS <licensing@enovance.com>
+#
+# Author: Frederic Lepied <frederic.lepied@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
 script="$1"
 actions="$2"
 level="$3"
-
-exec > /var/tmp/policy.out.$$ 2>&1
-
-echo "$@"
 
 if [ $level = unknown ]; then
    exit 101

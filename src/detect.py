redhat-cip/edeploy

commit e09080acd818d8fc6c64b71c22c2d21fd25006d9

detect.py: Adding more information about NIC devices
It could be very useful to select an NIC by its vendor/product/driver
name.

This allow people setting up rules where two nics shall be from the same
vendor for some HA setups etc..

src/detect.py
@@ -116,6 +116,12 @@ def detect_system(l, output=None):
                 serial = e.find('serial')
                 if serial is not None:
                     l.append(('network', name.text, 'serial', serial.text))
+		vendor = e.find('vendor')
+                if vendor is not None:
+                    l.append(('network', name.text, 'vendor', vendor.text))
+		product = e.find('product')
+		if product is not None:
+                    l.append(('network', name.text, 'product', product.text))
                 size = e.find('size')
                 if size is not None:
                     l.append(('network', name.text, 'size', size.text))
@@ -125,6 +131,10 @@ def detect_system(l, output=None):
                 link = e.find("configuration/setting[@id='link']")
                 if link is not None:
                     l.append(('network', name.text, 'link', link.attrib['value']))
+		driver = e.find("configuration/setting[@id='driver']")
+                if driver is not None:
+                    l.append(('network', name.text, 'driver', link.attrib['driver']))
+
     else:
         sys.stderr.write("Unable to run lshw: %s\n" % output)
 

commit 3c9033ea6ff8744984dbe0434adc866338e9a311

detect.py: Fixing typo while detecting network driver name
Commit e09080acd818d8fc6c64b71c22c2d21fd25006d9 introduced some wrong
code about the network driver matching.

A typo prevented the system from working.

src/detect.py
@@ -133,7 +133,7 @@ def detect_system(l, output=None):
                     l.append(('network', name.text, 'link', link.attrib['value']))
 		driver = e.find("configuration/setting[@id='driver']")
                 if driver is not None:
-                    l.append(('network', name.text, 'driver', link.attrib['driver']))
+                    l.append(('network', name.text, 'driver', driver.attrib['value']))
 
     else:
         sys.stderr.write("Unable to run lshw: %s\n" % output)
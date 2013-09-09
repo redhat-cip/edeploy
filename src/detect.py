b658fb565ae1308b30976122e84030e9cf84dd52
Erwan Velu <erwan@enovance.com>
2013-09-09 15:35:01 +0200

    detect.py: Don't fail if not disk vendor information
    Since last commit, we were failing if no disk's {vendor|model|rev} was
    existing. That's pretty weird but when using virtualized devices, it
    could happen.
    
    Let's be safe on this topic, let's ignore if the file reading fails.

--- src/detect.py
+++ src/detect.py
@@ -71,10 +71,13 @@ def detect_disks(hw_lst):
     sizes = diskinfo.disksizes(names)
     for name in [name for name, size in sizes.items() if size > 0]:
         hw_lst.append(('disk', name, 'size', str(sizes[name])))
-	item_list=['vendor','model','rev']
-	for my_item in item_list:
-		with open('/sys/block/%s/device/%s'%(name,my_item), 'r') as f:
-			hw_lst.append(('disk', name,my_item,f.readline().rstrip('\n').strip()))
+    item_list=['vendor','model','rev']
+    for my_item in item_list:
+        try:
+            with open('/sys/block/%s/device/%s'%(name,my_item), 'r') as f:
+                hw_lst.append(('disk', name,my_item,f.readline().rstrip('\n').strip()))
+        except:
+            True
 
 def modprobe(module):
     'Load a kernel module using modprobe.'

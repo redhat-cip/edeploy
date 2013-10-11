8e72b599109453ab36ddf63cd3a5f17ad2fb82b8
Erwan Velu <erwan@enovance.com>
2013-10-11 16:13:29 +0200

    netdetect: Switching network bw in Mbit/sec
    It's much more common to have network performances in Mbit/s instead of
    MB/sec

--- src/netdetect.py
+++ src/netdetect.py
@@ -400,7 +400,7 @@ def stop_bench_servers():
 
 def start_bench_client(ip, port):
     sys.stderr.write("Starting bench client on server %s:%s\n" % (ip, port))
-    cmd_netperf = subprocess.Popen('netperf -l %d -H %s -p %s -f M -t TCP_STREAM' % (int(BENCH_DURATION), ip, port),
+    cmd_netperf = subprocess.Popen('netperf -l %d -H %s -p %s -t TCP_STREAM' % (int(BENCH_DURATION), ip, port),
             shell=True, stdout=subprocess.PIPE)
 
 #    [root@localhost ~]# netperf -l 10 -H localhost  -f M -t TCP_STREAM

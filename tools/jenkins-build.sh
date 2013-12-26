5483ea6b8d77548770aeb6e01be109d0d15b2ea3
Frederic Lepied <frederic.lepied@enovance.com>
2013-12-26 11:25:52 +0100

    tools/jenkins-build.sh: archive roles if the target directory exists

--- tools/jenkins-build.sh
+++ tools/jenkins-build.sh
@@ -2,14 +2,8 @@
 
 SRC=$(cd $(dirname $0)/..; pwd)
 
-if [ $# -eq 0 ]; then
-    echo "$0 <dirname> [<make params>...]" 1>&2
-    exit 1
-fi
-
-
-if [ -z "$JOB_NAME" ]; then
-    echo "JOB_NAME env variable must be set" 1>&2
+if [ $# -lt 2 ]; then
+    echo "$0 <build dir> <archive dir> [<make params>...]" 1>&2
     exit 1
 fi
 
@@ -17,24 +11,38 @@ set -e
 
 DIR="$1"
 shift
+ARCH="$1"
+shift
 
 cleanup() {
     if [ -d "$DIR" ]; then
-	sudo rm -rf "$DIR"
+	sudo rm -rf "$DIR"/install/
     fi
 }
 
 trap cleanup 0
 
 if [ -z "$ROLES" ]; then
-    ROLES="pxe openstack-full puppet-master"
+    ROLES="base pxe openstack-common openstack-full puppet-master"
 fi
+
 set -x
 
 cd $SRC/build
 cleanup
-sudo mkdir -p "$DIR"/tmp
+sudo mkdir -p "$DIR"/install
+RC=0
 for role in $ROLES; do
-    sudo make TOP="$DIR" VERS=$JOB_NAME NO_COMPRESSED_FILE=1 "$@" $role || exit 1
-    sudo rm -rf "$DIR"/install/$JOB_NAME/$role*
+    if sudo make TOP="$DIR" "$@" $role; then
+	if [ -d "$ARCH" ]; then
+	    VERS=$(sudo make TOP="$DIR" "$@" version)
+	    mkdir -p "$ARCH"/$VERS/
+	    rsync -a "$DIR"/install/$VERS/*.* "$ARCH"/$VERS/
+	    git rev-parse HEAD > "$ARCH"/$VERS/$role.rev
+	fi
+    else
+	RC=1
+    fi
 done
+
+exit $RC

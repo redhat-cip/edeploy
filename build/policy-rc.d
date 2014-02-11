568e151cc324c2cfacc85a540b5ee746c95b7b51
Frederic Lepied <frederic.lepied@enovance.com>
2014-02-11 08:20:58 +0100

    build/policy-rc.d: add more intelligence to detect if we are called from a package scriptlet or outside

--- build/policy-rc.d
+++ build/policy-rc.d
@@ -16,10 +16,31 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+# Purpose: prevent services to start or do whatever at installation time.
+
 script="$1"
 actions="$2"
 level="$3"
 
-if [ $actions != rotate ]; then
+# try to detect that we are called from a package scriptlet
+if [ -n "$PPID" ]; then
+    PPPID=$(ps -p $PPID -o ppid|head -2|tail -1|sed -e 's/^\s*//' -e 's/\s*$//')
+    #cat /proc/$PPPID/cmdline|tr '\000' '\n' 1>&2
+    set -- $(cat /proc/$PPPID/cmdline|tr '\000' '\n')
+    if [ "$2" = -e ]; then
+	SCRIPT="$3"
+    else
+	SCRIPT="$2"
+    fi
+    case "$SCRIPT" in
+	*.postinst|*.preinst|*.postrm|*.prerm)
+	    echo "policy-rc.d: scriptlet detected: preventing action $actions on $script" 1>&2
+	    exit 101
+	    ;;
+	*)
+	    echo "policy-rc.d: called outside of a scriptlet: action $actions on $script authorized" 1>&2
+	    ;;
+    esac
+elif [ "$actions" != rotate ]; then
     exit 101
 fi

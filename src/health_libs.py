87238fef29a82897f0c66ad7836705beef0baa7e
Erwan Velu <erwan.velu@enovance.com>
2014-04-17 15:25:56 +0200

    health: Using decode() on read line
    This is mandatory on python3 unless we do have exceptions.

--- src/health_libs.py
+++ src/health_libs.py
@@ -20,12 +20,12 @@ def run_sysbench(hw_, max_time, cpu_count, processor_num=-1):
         taskset = 'taskset %s' % hex(1 << processor_num)
 
     cmds = '%s sysbench --max-time=%d --max-requests=1000000 ' \
-           '--num-threads=%d --test=cpu --cpu-max-prime=15000 run'
-    sysbench_cmd = subprocess.Popen(cmds % (taskset, max_time, cpu_count),
-                                    shell=True, stdout=subprocess.PIPE)
+           '--num-threads=%d --test=cpu --cpu-max-prime=15000 run' % (taskset, max_time, cpu_count)
+    sysbench_cmd = subprocess.Popen(cmds, shell=True, stdout=subprocess.PIPE)
+
     for line in sysbench_cmd.stdout:
-        if "total number of events" in line:
-            title, perf = line.rstrip('\n').replace(' ', '').split(':')
+        if "total number of events" in line.decode():
+            title, perf = line.decode().rstrip('\n').replace(' ', '').split(':')
             if processor_num == -1:
                 hw_.append(('cpu', 'logical', 'loops_per_sec',
                             str(int(perf) / max_time)))

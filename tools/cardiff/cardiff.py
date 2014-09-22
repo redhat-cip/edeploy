f7a48a406b61ed54724545a3999417b4bf2e66f1
Erwan Velu <erwan.velu@enovance.com>
2014-09-22 14:38:59 +0200

    Revert "cardiff: Fixing hypervisors computation"
    This reverts commit 8439e3c30b91d86956b7bb4817c512124219ef16.
    Affinity was not properly set for the network test.

--- tools/cardiff/cardiff.py
+++ tools/cardiff/cardiff.py
@@ -306,7 +306,7 @@ def plot_results(current_dir, rampup_values, job, metrics, bench_values):
                 extract_hw_info(bench_values[0], 'system', 'kernel', 'version')[0],
                 extract_hw_info(bench_values[0], 'system', 'kernel', 'arch')[0])
 
-        subtitle = "\\nBenchmark setup : %s, runtime=%d seconds, %d hypervisors with %s scheduling\\n%s" % (context, metrics["bench"]["runtime"], len(metrics["bench"]["affinity-hosts"]), metrics["bench"]["affinity"], system)
+        subtitle = "\\nBenchmark setup : %s, runtime=%d seconds, %d hypervisors with %s scheduling\\n%s" % (context, metrics["bench"]["runtime"], len(metrics["affinity"]), metrics["bench"]["affinity"], system)
 
         if kind in expected_value:
             do_plot(current_dir, gpm_dir, title, subtitle, kind, unit[kind], expected_value[kind])

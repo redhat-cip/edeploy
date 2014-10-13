f1a1818d2e4cd181912e602fc0c7d30802f673e3
Erwan Velu <erwan.velu@enovance.com>
2014-10-13 17:17:45 +0200

    detect: Reporting current LLDP configuration per interface
    When detecting the hardware, let's report what is the LLDP information
    reported by the switch.
    
    It is now possible to match some hardware based of its low-level network configuration (port location, switch name, available VLANs, ...)
    
    The reported information are really depending from the switch model/version and its configuration.
    This patch will report everything found from the "lldaptool -n -t -i <interface>" output under the following format:
        'lldp', '<nic_name>', 'header, 'value'
    
    The default parsing of lldaptool is turned into a 2 field reporting.
    If multiple levels are found, they will be turned into "x/y" format in the header.
    
    A typical output looks like:
     ('lldp', 'eth3', 'Chassis ID/MAC', 'f8:b1:56:15:e6:c6'),
     ('lldp', 'eth3', 'Port ID/Ifname', 'gi3_4'),
     ('lldp', 'eth3', 'Time to Live', '120'),
     ('lldp', 'eth3', 'MAC_PHY Configuration Status', 'Auto-negotiation supported and enabled'),
     ('lldp', 'eth3', 'MAC_PHY Configuration Status/PMD auto-negotiation capabilities', '0x6c01'),
     ('lldp', 'eth3', 'MAC_PHY Configuration Status/PMD auto-negotiation capabilities/MAU type', '1000 BaseTFD'),
     ('lldp', 'eth3', 'Port Description', 'gigabitethernet3_4'),
     ('lldp', 'eth3', 'System Name', 'Switch POD'),
     ('lldp', 'eth3', 'System Description', 'R1-2401 VRTX 1Gb Switch Module'),
     ('lldp', 'eth3', 'System Capabilities/System capabilities', ' Bridge'),
     ('lldp', 'eth3', 'System Capabilities/System capabilities/Enabled capabilities', 'Bridge'),

--- src/detect.py
+++ src/detect.py
@@ -498,6 +498,29 @@ def detect_system(hw_lst, output=None):
                     find_element(elt, 'serial', 'serial', name.text, 'network',
                                  transform=string.lower)
 
+                lines = output_lines("lldptool -t -n -i %s" % name.text)
+                content = ""
+                header = ""
+                sub_header = ""
+                for line in lines:
+                    if line.startswith('\t'):
+                        content = line.strip().strip('\n').strip('\t').replace("/", "_")
+                    else:
+                        header = line
+                        header = line.strip().strip('\n').strip('\t').replace("/", "_").replace(" TLV", "")
+                        content = ""
+                        sub_header = ""
+                    if header and content:
+                        if ":" in content:
+                            left, right = content.split(": ")
+                            # If we never had this sub_header for this header
+                            # let's add one
+                            if (left != sub_header):
+                                sub_header = left
+                                header = header + "/" + sub_header
+                            content = right
+                        hw_lst.append(('lldp', name.text, header, content))
+
         for elt in xml.findall(".//node[@class='processor']"):
             name = elt.find('physid')
             if name is not None:

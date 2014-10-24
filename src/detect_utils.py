8fc4306a54086f749234d8e4d5c439418ec64bdb
Erwan Velu <erwan.velu@enovance.com>
2014-10-24 18:38:58 +0200

    detect: Fixing lldp parsing code
    On some lldp parsing, we have subheaders in subheaders. The existing
    code didn't supported it and failed with a python exception.
    
    This patch is insuring this kind of output is perfectly handled without
    any regression.

--- src/detect_utils.py
+++ src/detect_utils.py
@@ -15,13 +15,20 @@ def parse_lldtool(hw_lst, interface_name, lines):
             sub_header = ""
         if header and content:
             if ":" in content:
-                left, right = content.split(": ")
-                # If we never had this sub_header for this header
-                # let's add one
-                if (left != sub_header):
-                    sub_header = left
+                line = content.split(":")
+                if (len(line) == 2) and (line[1] == ''):
+                    sub_header = line[0].strip().strip('\n').strip('\t').replace("/", "_"). replace(" TLV:", "")
                     header = header + "/" + sub_header
-                content = right
+                    continue
+                else:
+                    left = line[0].strip().strip('\n').strip('\t').replace("/", "_")
+                    right = content.replace(left+":", "").strip().strip('\n').strip('\t').replace("/", "_")
+                    # If we never had this sub_header for this header
+                    # let's add one
+                    if (left != sub_header):
+                        sub_header = left
+                        header = header + "/" + sub_header
+                    content = right
             hw_lst.append(('lldp', interface_name, header, content))
 
     return hw_lst

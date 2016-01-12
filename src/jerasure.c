69df6944c8ab2257a73448a3822bb2d2bf62a072
Erwan Velu <erwan@redhat.com>
2016-01-12 13:53:33 +0100

    jerasure: Remove memleak in jerasure_matrix_to_bitmatrix()
    When matrix is NULL, the previously allocated bitmatrix is not freed leading to
    a memleak.
    
    This patch simply add a free() call before the return.

--- src/jerasure.c
+++ src/jerasure.c
@@ -275,7 +275,10 @@ int *jerasure_matrix_to_bitmatrix(int k, int m, int w, int *matrix)
   int rowelts, rowindex, colindex, elt, i, j, l, x;
 
   bitmatrix = talloc(int, k*m*w*w);
-  if (matrix == NULL) { return NULL; }
+  if (matrix == NULL) {
+    free(bitmatrix);
+    return NULL;
+  }
 
   rowelts = k * w;
   rowindex = 0;

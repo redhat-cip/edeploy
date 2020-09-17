7f233471f58872d073fb1bdd515dabd446a950a3
Erwan Velu <e.velu@criteo.com>
2020-09-17 15:19:09 +0200

    health-libs: Adding io_uring support
    This commit tests both libaio & io-uring if both engines are available.
    
    Signed-off-by: Erwan Velu <e.velu@criteo.com>

--- src/health_libs.py
+++ src/health_libs.py
@@ -497,65 +497,75 @@ def run_fio(hw_, disks_list, mode, io_size, time, rampup_time):
     filelist = [f for f in os.listdir(".") if f.endswith(".fio")]
     for myfile in filelist:
         os.remove(myfile)
-    fio = "fio --ioengine=libaio --invalidate=1 --ramp_time=%d --iodepth=32 " \
-          "--runtime=%d --time_based --direct=1 " \
-          "--bs=%s --rw=%s" % (rampup_time, time, io_size, mode)
 
-    global_disk_list = ''
-    for disk in disks_list:
-        if '/dev/' not in disk:
-            disk = '/dev/%s' % disk
-        # Flusing Disk's cache prior benchmark
-        os.system("hdparm -f %s >/dev/null 2>&1" % disk)
-        short_disk = disk.replace('/dev/', '')
-        fio = "%s --name=MYJOB-%s --filename='%s'" % (fio, short_disk, disk)
-        global_disk_list += '%s,' % short_disk
-    global_disk_list = global_disk_list.rstrip(',')
-    sys.stderr.write(
-        'Benchmarking storage %s for %s seconds in '
-        '%s mode with blocksize=%s\n' %
-        (global_disk_list, time, mode, io_size))
-    fio_cmd = subprocess.Popen(fio,
-                               shell=True, stdout=subprocess.PIPE)
-    current_disk = ''
-    for line in fio_cmd.stdout:
-        if ('MYJOB-' in line) and ('pid=' in line):
-            # MYJOB-sda: (groupid=0, jobs=1): err= 0: pid=23652: Mon Sep  9
-            # 16:21:42 2013
-            current_disk = re.search('MYJOB-(.*): \(groupid', line).group(1)
+    for engine in ['libaio', 'io_uring']:
+        # Let's check if fio supports the required engines
+        if subprocess.call(['fio', '--enghelp={}'.format(engine)]) != 0:
+            print('fio engine {} is not available'.format(engine))
             continue
-        if ("read: IOPS=" in line) or ("write: IOPS=" in line):
-            # read: IOPS=225, BW=225MiB/s (236MB/s)(255MiB/1133msec)\n'
-            if (len(disks_list) > 1):
-                mode_str = "simultaneous_%s_%s" % (mode, io_size)
-            else:
-                mode_str = "standalone_%s_%s" % (mode, io_size)
 
-            try:
-                perf = re.search('BW=(.*?B/s)', line).group(1)
-            except Exception:
-                sys.stderr.write('Failed at detecting '
-                                 'bwps pattern with %s\n' % line)
-            else:
-                multiply = 1
-                divide = 1
-                if "MiB/s" in perf:
-                    multiply = 1000
-                elif "KiB/s" in perf:
-                    multiply = 1
-                elif "B/s" in perf:
-                    divide = 1000
+        fio = 'fio --ioengine={} --invalidate=1 --ramp_time={} --iodepth=32 ' \
+              '--runtime={} --time_based --direct=1 ' \
+              '--bs={} --rw={}'.format(engine, rampup_time,
+                                       time, io_size, mode)
+
+        global_disk_list = ''
+        for disk in disks_list:
+            if '/dev/' not in disk:
+                disk = '/dev/%s' % disk
+            # Flusing Disk's cache prior benchmark
+            os.system("hdparm -f %s >/dev/null 2>&1" % disk)
+            short_disk = disk.replace('/dev/', '')
+            fio = "%s --name=MYJOB-%s --filename='%s'" % (
+                fio, short_disk, disk)
+            global_disk_list += '%s,' % short_disk
+        global_disk_list = global_disk_list.rstrip(',')
+        sys.stderr.write(
+            'Benchmarking storage {} for {} seconds in {} mode with blocksize={} (engine={})\n'.format(
+                global_disk_list, time, mode, io_size, engine))
+        fio_cmd = subprocess.Popen(fio, shell=True, stdout=subprocess.PIPE)
+        current_disk = ''
+        for line in fio_cmd.stdout:
+            if ('MYJOB-' in line) and ('pid=' in line):
+                # MYJOB-sda: (groupid=0, jobs=1): err= 0: pid=23652: Mon Sep  9
+                # 16:21:42 2013
+                current_disk = re.search(
+                    'MYJOB-(.*): \(groupid', line).group(1)
+                continue
+            if ("read: IOPS=" in line) or ("write: IOPS=" in line):
+                # read: IOPS=225, BW=225MiB/s (236MB/s)(255MiB/1133msec)\n'
+                if (len(disks_list) > 1):
+                    mode_str = '{}_simultaneous_{}_{}'.format(
+                        engine, mode, io_size)
+                else:
+                    mode_str = '{}_standalone_{}_{}'.format(
+                        engine, mode, io_size)
+
                 try:
-                    iperf = perf.replace(
-                        'KiB/s', '').replace('MiB/s', '').replace('B/s', '')
+                    perf = re.search('BW=(.*?B/s)', line).group(1)
                 except Exception:
-                    True
-                hw_.append(('disk', current_disk, mode_str + '_KiBps',
-                            str(int(float(float(iperf) * multiply / divide)))))
+                    sys.stderr.write('Failed at detecting '
+                                     'bwps pattern with %s\n' % line)
+                else:
+                    multiply = 1
+                    divide = 1
+                    if "MiB/s" in perf:
+                        multiply = 1000
+                    elif "KiB/s" in perf:
+                        multiply = 1
+                    elif "B/s" in perf:
+                        divide = 1000
+                    try:
+                        iperf = perf.replace(
+                            'KiB/s', '').replace('MiB/s', '').replace('B/s', '')
+                    except Exception:
+                        True
+                    hw_.append(('disk', current_disk, mode_str + '_KiBps',
+                                str(int(float(float(iperf) * multiply / divide)))))
 
-            try:
-                hw_.append(('disk', current_disk, mode_str + '_IOps',
-                            re.search('IOPS=(.*),', line).group(1).strip(' ')))
-            except Exception:
-                sys.stderr.write('Failed at detecting iops '
-                                 'pattern with %s\n' % line)
+                try:
+                    hw_.append(('disk', current_disk, mode_str + '_IOps',
+                                re.search('IOPS=(.*),', line).group(1).strip(' ')))
+                except Exception:
+                    sys.stderr.write('Failed at detecting iops '
+                                     'pattern with %s\n' % line)
